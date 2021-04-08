0b5343afd122d055a792ae8ec1069753e9b7bc65
CyberPunk_xiaomi <konstantin21_arx@bk.ru>
2021-04-08 18:08:05 +0300

    Add client vue

--- server.js
+++ server.js
@@ -1,43 +1,38 @@
-const { ApolloServer, gql } = require("apollo-server");
+const { ApolloServer } = require('apollo-server')
+const mongoose = require('mongoose')
+const fs = require('fs')
+const path = require('path')
 
-const todos = [
-  { task: "Wash car", completed: false },
-  { task: "Clean room", completed: true }
-];
+// import typeDefs and resolvers
+const filePath = path.join(__dirname, 'typeDefs.gql')
+const typeDefs = fs.readFileSync(filePath, 'utf-8')
+const resolvers = require('./resolvers')
 
-const typeDefs = gql`
-  type Todo {
-    task: String
-    completed: Boolean
-  }
+// import Environment Variables and Mongoose models
+require('dotenv').config({ path: 'variables.env' })
+const User = require('./models/User')
+const Post = require('./models/Post')
 
-  type Query {
-    getTodos: [Todo]
-  }
+// Connect to mongoDB
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+  })
+  .then(console.log('MongoDB connected...'))
+  .catch(error => console.error('MongoDB disconnected...', error))
 
-  type Mutation {
-    addTodo(task: String, completed: Boolean): Todo
-  }
-`;
-
-const resolvers = {
-  Query: {
-    getTodos: () => todos
-  },
-  Mutation: {
-    addTodo: (_, { task, completed }) => {
-      const todo = { task, completed };
-      todos.push(todo);
-      return todo;
-    }
-  }
-};
-
+// Create Apollo/GraphQl server using typeDefs, resolvers and context object
 const server = new ApolloServer({
   typeDefs,
-  resolvers
-});
+  resolvers,
+  context: {
+    User,
+    Post
+  }
+})
 
 server.listen().then(({ url }) => {
-  console.log(`Server listening on ${url}`);
-});
+  console.log(`Server listening on ${url}`)
+})
